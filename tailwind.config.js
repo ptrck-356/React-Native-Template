91bec039c0002b357b77f0e2e51fa69edb820801
Your Name <you@example.com>
2026-06-11 20:35:02 +0800

    feat: update configuration files for NativeWind integration and add generated type definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
